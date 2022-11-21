36e58f718573a462166511b86ce47865bdf1d365
David Swarbrick <david.swarbrick9@gmail.com>
2022-11-21 11:17:34 +0000

    Add read/write with feature toggle

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,29 @@
-// use embedded_hal::i2c;
-// #[cfg(not(test))] no_std
+#![cfg_attr(not(test), no_std)]
 
+use embedded_hal::i2c::{I2c};
+
+const ADDR: u8 = 0;
+#[cfg(feature = "register-buffer")]
 static WM8978_DEFAULT_REG: [u16; 58] = [0x0000, 0x0000, 0x0000, 0x0000, 0x0050, 0x0000, 0x0140, 0x0000, 0x0000, 0x0000,
                 0x0000, 0x00FF, 0x00FF, 0x0000, 0x0100, 0x00FF, 0x00FF, 0x0000, 0x012C, 0x002C,
                 0x002C, 0x002C, 0x002C, 0x0000, 0x0032, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
                 0x0000, 0x0000, 0x0038, 0x000B, 0x0032, 0x0000, 0x0008, 0x000C, 0x0093, 0x00E9,
                 0x0000, 0x0000, 0x0000, 0x0000, 0x0003, 0x0010, 0x0010, 0x0100, 0x0100, 0x0002,
                 0x0001, 0x0001, 0x0039, 0x0039, 0x0039, 0x0039, 0x0001, 0x0001,];
+
+
+
+/// Error type combining SPI, I2C, and Pin errors
+/// You can remove anything you don't need / add anything you do
+/// (as well as additional driver-specific values) here
+#[derive(Debug, Clone, PartialEq)]
+pub enum DriverError<I2cError> {
+    /// Underlying I2C device error
+    I2c(I2cError),
+    OutOfBounds,
+    /// Device failed to resume from reset
+    ResetTimeout
+}
 
 pub struct Wm8978Driver<I2C, Mode>
 // where
@@ -17,12 +34,20 @@ pub struct Wm8978Driver<I2C, Mode>
     _mode: Mode,
 }
 
-impl<I2C, Mode> Wm8978Driver<I2C, Mode> {
+impl<I2C,I2cError, Mode> Wm8978Driver<I2C, Mode>
+where
+    I2C: I2c<Error = I2cError>,
+{
     // ToDo: raise appropriate error rather than Option
-    pub fn read_reg(&self, reg: usize) -> Option<&u16> {
-        self.registers.get(reg)
+    #[cfg(feature = "register-buffer")]
+    pub fn read_reg(&self, reg: usize) -> Result<&u16, DriverError<I2cError>> {
+        self.registers.get(reg).ok_or(DriverError::OutOfBounds)
     }
-    pub fn write_reg(&self, reg: usize, val: u16) {
+    #[cfg(feature = "register-buffer")]
+    pub fn write_reg(& mut self, reg: usize, val: u16) ->Result<(), DriverError<I2cError>> {
+        let mut r = self.registers.get_mut(reg).ok_or(DriverError::OutOfBounds)?;
+        *r = val;
+        Ok(())
         // ToDo: Implement i2c write, using trait.
     }
 }
